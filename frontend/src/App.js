d727f7809997c76c719756b510dbb64c45b3c017
MoazHassan2022 <87096647+MoazHassan2022@users.noreply.github.com>
2023-06-06 05:19:20 +0200

    finished frontend part

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -75,6 +75,46 @@ const Error = styled.p`
   color: red;
 `;
 
+const Table = styled.table`
+  width: 100%;
+  margin-top: 2rem;
+  border-collapse: collapse;
+`;
+
+const TableHead = styled.thead`
+  background-color: #f8f9fa;
+`;
+
+const TableHeader = styled.th`
+  padding: 1rem;
+  font-weight: bold;
+  text-align: left;
+`;
+
+const TableBody = styled.tbody``;
+
+const TableRow = styled.tr`
+  &:nth-child(even) {
+    background-color: #f8f9fa;
+  }
+`;
+
+const TableCell = styled.td`
+  padding: 1rem;
+  border-bottom: 1px solid #dee2e6;
+  max-width: 150px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  cursor: pointer;
+
+  &:hover {
+    overflow: visible;
+    white-space: normal;
+    max-width: none;
+  }
+`;
+
 const App = () => {
   const [url, setUrl] = useState("");
   const [slug, setSlug] = useState("");
@@ -84,12 +124,124 @@ const App = () => {
   const [iosFallback, setIOSFallback] = useState("");
   const [androidPrimary, setAndroidPrimary] = useState("");
   const [androidFallback, setAndroidFallback] = useState("");
+  const [urlUpdate, setUrlUpdate] = useState("");
+  const [slugUpdate, setSlugUpdate] = useState("");
+  const [errorUpdate, setErrorUpdate] = useState("");
+  const [iosPrimaryUpdate, setIOSPrimaryUpdate] = useState("");
+  const [iosFallbackUpdate, setIOSFallbackUpdate] = useState("");
+  const [androidPrimaryUpdate, setAndroidPrimaryUpdate] = useState("");
+  const [androidFallbackUpdate, setAndroidFallbackUpdate] = useState("");
+  const [errorShow, setErrorShow] = useState("");
+  const [shortLinks, setShortLinks] = useState([]);
+  const [showTable, setShowTable] = useState(false);
+
+  const fetchShortLinks = async () => {
+    try {
+      const response = await fetch("/shortlinks");
+      const data = await response.json();
+      /*const data = [  {
+          slug: "cedcedced5",
+          web: "https://google.com",
+          ios: {
+            primary: "https://apps.apple.com/us/app/instagram/id389801252",
+            fallback: "https://apps.apple.com/us/app/instagram/id389801252",
+          },
+          android: {
+            primary: "https://apps.store.com/us/app/instagram/id389801252",
+            fallback: "https://apps.store.com/us/app/instagram/id389801252",
+          },
+        },
+      ];*/
+      setShortLinks(data);
+      setShowTable(true);
+    } catch (error) {
+      console.error(error);
+      setErrorShow("Server error");
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:3000/shortlinks", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          slug,
+          ios: {
+            primary: iosPrimary,
+            fallback: iosFallback,
+          },
+          android: {
+            primary: androidPrimary,
+            fallback: androidFallback,
+          },
+          web: url,
+        }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setShortLink(`http://localhost:3000/${slug}`);
+        setError("");
+      } else {
+        const error = await response.json();
+        setShortLink("");
+        setError(error.error);
+      }
+    } catch (error) {
+      console.error(error);
+      setShortLink("");
+      setError("Server error");
+    }
+  };
+
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(`/shortlinks/${slugUpdate}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          ios: {
+            primary: iosPrimaryUpdate,
+            fallback: iosFallbackUpdate,
+          },
+          android: {
+            primary: androidPrimaryUpdate,
+            fallback: androidFallbackUpdate,
+          },
+          web: urlUpdate, // Assuming updateUrl is the new URL input for web
+        }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setShortLink(`http://localhost:3000/${slugUpdate}`);
+        setError("");
+      } else {
+        const error = await response.json();
+        setShortLink("");
+        setErrorUpdate(error.error);
+      }
+    } catch (error) {
+      console.error(error);
+      setShortLink("");
+      setErrorUpdate("Server error");
+    }
+  };
 
   return (
     <Wrapper>
       <Logo src={logo} alt="Logo" />
       <Title>Let's get your URL shortened!</Title>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Input
           type="text"
           value={url}
@@ -138,41 +290,41 @@ const App = () => {
       )}
       {error && <Error>{error}</Error>}
       <Title>Update your old shortened URL!</Title>
-      <Form>
+      <Form onSubmit={handleUpdate}>
         <Input
           type="text"
-          value={slug}
-          onChange={(e) => setSlug(e.target.value)}
+          value={slugUpdate}
+          onChange={(e) => setSlugUpdate(e.target.value)}
           placeholder="Old Slug"
         />
         <Input
           type="text"
-          value={url}
-          onChange={(e) => setUrl(e.target.value)}
+          value={urlUpdate}
+          onChange={(e) => setUrlUpdate(e.target.value)}
           placeholder="Enter URL (Optional)"
         />
         <Input
           type="text"
-          value={iosPrimary}
-          onChange={(e) => setIOSPrimary(e.target.value)}
+          value={iosPrimaryUpdate}
+          onChange={(e) => setIOSPrimaryUpdate(e.target.value)}
           placeholder="iOS Primary (Optional)"
         />
         <Input
           type="text"
-          value={iosFallback}
-          onChange={(e) => setIOSFallback(e.target.value)}
+          value={iosFallbackUpdate}
+          onChange={(e) => setIOSFallbackUpdate(e.target.value)}
           placeholder="iOS Fallback (Optional)"
         />
         <Input
           type="text"
-          value={androidPrimary}
-          onChange={(e) => setAndroidPrimary(e.target.value)}
+          value={androidPrimaryUpdate}
+          onChange={(e) => setAndroidPrimaryUpdate(e.target.value)}
           placeholder="Android Primary (Optional)"
         />
         <Input
           type="text"
-          value={androidFallback}
-          onChange={(e) => setAndroidFallback(e.target.value)}
+          value={androidFallbackUpdate}
+          onChange={(e) => setAndroidFallbackUpdate(e.target.value)}
           placeholder="Android Fallback (Optional)"
         />
         <Button type="submit">Update</Button>
@@ -185,7 +337,38 @@ const App = () => {
           </a>
         </ShortLink>
       )}
-      {error && <Error>{error}</Error>}
+      {errorUpdate && <Error>{errorUpdate}</Error>}
+      <br></br>
+      <br></br>
+      <br></br>
+      <Button onClick={fetchShortLinks}>Show Short Links</Button>
+      {errorShow && <Error>{errorShow}</Error>}
+      {showTable && (
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableHeader>Slug</TableHeader>
+              <TableHeader>Web</TableHeader>
+              <TableHeader>IOS Primary</TableHeader>
+              <TableHeader>IOS Fallback</TableHeader>
+              <TableHeader>Android Primary</TableHeader>
+              <TableHeader>Android Fallback</TableHeader>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {shortLinks.map((link) => (
+              <TableRow key={link.slug}>
+                <TableCell>{link.slug}</TableCell>
+                <TableCell>{link.web}</TableCell>
+                <TableCell>{link.ios.primary}</TableCell>
+                <TableCell>{link.ios.fallback}</TableCell>
+                <TableCell>{link.android.primary}</TableCell>
+                <TableCell>{link.android.fallback}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      )}
     </Wrapper>
   );
 };
